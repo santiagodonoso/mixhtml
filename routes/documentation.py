dd41b1fca10defcd0906aa8ed7e11be80574c0f6
Santiago Donoso <santiagodonoso@gmail.com>
2024-01-30 23:01:14 +0100

    .

--- routes/documentation.py
+++ routes/documentation.py
@@ -4,8 +4,13 @@ from bottle import get, request, template
 
 @get("/documentation")
 def _():
+    url = "/documentation"
+    documentation_active_link = "active_nav_link"
+    nav_middle_id = "nav_documentation"
     is_spa = True if request.query.get("spa") else False
-    return template("documentation.html", is_spa = is_spa)
+    return template("documentation.html", is_spa = is_spa, 
+                    documentation_active_link=documentation_active_link,
+                    nav_middle_id=nav_middle_id, url=url)
 
 
 
--- routes/index.py
+++ routes/index.py
@@ -3,6 +3,9 @@ from bottle import get, request, template
 ##############################
 @get("/")
 def _():
+    url = "/"
+    index_active_link = "active_nav_link"
+    nav_middle_id = "nav_index"
     is_spa = True if request.query.get("spa") else False
     items = [
         {"id":1, "name":"one", "color":"#737373"},
@@ -10,7 +13,9 @@ def _():
         {"id":3, "name":"three", "color":"##737373"},
         {"id":4, "name":"four", "color":"#737373"},
     ]
-    return template("index.html", items=items, is_spa=is_spa)
+    return template("index.html", items=items, is_spa=is_spa, 
+                    index_active_link=index_active_link,
+                    nav_middle_id = nav_middle_id, url=url)
 
 
 
